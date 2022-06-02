a5156c3c68226ce1c732798acea3b623fbd9dece
cristianpantaziu <cascau@rocketmail.com>
2022-06-02 21:26:00 +0300

    Initial commit

---
+++ src/main/java/com/tonks/res/webscraper/anuntul/ResWebScraperAnuntulApplication.java
@@ -0,0 +1,13 @@
+package com.tonks.res.webscraper.anuntul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResWebScraperAnuntulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResWebScraperAnuntulApplication.class, args);
+	}
+
+}
